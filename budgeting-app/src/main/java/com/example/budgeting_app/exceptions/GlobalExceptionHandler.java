0bbae3baf27ea40a45edea312086efc487ad87ac
barakavic <barakavictor397@gmail.com>
2025-10-12 15:27:33 +0300

    feat: Implement GlobalExceptionHandler and ResourceNotFoundException for improved error handling

---
+++ budgeting-app/src/main/java/com/example/budgeting_app/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.example.budgeting_app.exceptions;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleResourceNotFoundException(ResourceNotFoundException ex){
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("error", ex.getMessage());
+        response.put("status", HttpStatus.NOT_FOUND.value());
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+
+
+
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleGeneralException(Exception ex){
+        Map<String,Object> response = new HashMap<>();
+
+        response.put("timestamp", LocalDateTime.now());
+        response.put("error", ex.getMessage());
+        response.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    
+}
---
+++ budgeting-app/src/main/java/com/example/budgeting_app/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.budgeting_app.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message){
+        super(message);
+
+    }
+    
+}
