91d96da2a054b37c872ab14d2f5383a0507086ee
barakavic <barakavictor397@gmail.com>
2025-10-16 22:48:13 +0300

    CreatedFeatue: Added category service to call the category api and also added the category details to the main app

--- ba_102_fe/lib/features/categories/presentation/categories_page.dart
+++ ba_102_fe/lib/features/categories/presentation/categories_page.dart
@@ -1,23 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ba_102_fe/data/api/categoryService.dart';
+import 'package:ba_102_fe/data/models/models.dart';
+
+final CategriesProvider = FutureProvider<List<Category>>((ref) async{
+  return Categoryservice().fetchCategories();
+});
+
 
 class CategoriesPage extends ConsumerWidget{
   const CategoriesPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+
+    final categoriesAsync = ref.watch(CategriesProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Categories"),
         centerTitle: true,
       ),
 
-      body: const Center (
-        child: Text("Your Spending Categories will appear here.",
-        style: TextStyle(
-          fontSize: 16.0,
-        ),),
+      body: categoriesAsync.when(
+        data: (categories){
+          if (categories.isEmpty){
+            return const Center(
+              child:
+               Text('No categories found'),);
+
+          }
+
+          return Padding(padding: const EdgeInsets.all(16.0),
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,
+            crossAxisSpacing: 8,
+            mainAxisSpacing: 8,
+            childAspectRatio: 1.2,
+            ), 
+            itemCount: categories.length,
+            itemBuilder: (context, index){
+              final category = categories[index];
+              return Card(
+                elevation: 2,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+
+                child: Center(
+                  child: Text(
+                    category.name,
+                    style: const TextStyle(
+                      
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600
+                    ),
+                  ),
+                ),
+              );
+              
+
+            },
+
+          
+            ),);
+
+
+        },
+        loading: () => const Center(child: CircularProgressIndicator(),),
+        error: (err, stack) =>Center(child: Text('Error: $err'),)
       ),
+
+ 
 
     );
     
