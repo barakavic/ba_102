70e13e4aed5b051b819cc0434fb726dc72f8b29e
barakavic <barakavictor397@gmail.com>
2025-10-20 23:25:44 +0300

    fixed the schema for the SQLite schema .sql to match the imported csvs

--- budgeting-app/schema.sql
+++ budgeting-app/schema.sql
@@ -1,17 +1,20 @@
 
 CREATE TABLE budget_category (
-    id INTEGER NOT NULL,
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     limit_amount REAL,
     spent_amount REAL,
+    status TEXT,
     plan_id INTEGER,
     FOREIGN KEY (plan_id) REFERENCES budget_plans(id)
 );
+
+DROP TABLE IF EXISTS budget_plans;
 CREATE TABLE budget_plans (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    end_date TEXT,
     name TEXT,
     start_date TEXT,
+    end_date TEXT,
     total_amount REAL,
     status TEXT
     );
@@ -19,11 +22,12 @@ CREATE TABLE budget_plans (
 
 CREATE TABLE transactions (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    description TEXT,
     amount REAL,
     date TEXT,
-    description TEXT,
     category_id INTEGER,
     plan_id INTEGER,
     FOREIGN KEY (category_id) REFERENCES budget_category(id),
     FOREIGN KEY (plan_id) REFERENCES budget_plans(id)
 );
+
