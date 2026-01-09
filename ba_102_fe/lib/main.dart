e92857a3be28d8dda6e3689c25e29dd5ede9fd62
barakavic <barakavictor397@gmail.com>
2026-01-09 12:05:29 +0300

    feat: Implement BootReceiver to restart SMS listener on device boot; request background permissions in main application

--- ba_102_fe/lib/main.dart
+++ ba_102_fe/lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:ba_102_fe/data/local/database_helper.dart';
-import 'package:ba_102_fe/data/local/plan_ls.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ba_102_fe/main_navigation.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+
+Future <void> requestBackgroundPermissions() async{
+  if(await Permission.ignoreBatteryOptimizations.isDenied){
+    await Permission.ignoreBatteryOptimizations.request();
+  }
+}
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await DatabaseHelper.instance.database;
+  await requestBackgroundPermissions();
 
   
 
@@ -20,7 +28,7 @@ class BudgetingApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Budgeting App',
+      title: 'HoneyBadger',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple.shade700),
         useMaterial3: true,
