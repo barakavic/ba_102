f49ab211f758fda42c333132c564934cfe91035d
barakavic <barakavictor397@gmail.com>
2026-01-10 13:21:55 +0300

    feat: dockerize backend and database with environment-based configuration

--- budgeting-app/src/main/java/com/example/budgeting_app/config/EnvConfig.java
+++ budgeting-app/src/main/java/com/example/budgeting_app/config/EnvConfig.java
@@ -4,13 +4,13 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
 public class EnvConfig {
     @Bean
-    public Dotenv dotenv(){
-        return Dotenv.load();
+    public Dotenv dotenv() {
+        return Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
     }
 
-    
 }
